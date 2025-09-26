a72bbe4473d0803037256dc85c43915267ff62ec
Adam Maxwell <maxwell7774@gmail.com>
2025-09-26 01:11:38 -0600

    Adds a theme, ts_ls, and prettier config

--- nvim/lua/stitch/lazy/lsp.lua
+++ nvim/lua/stitch/lazy/lsp.lua
@@ -112,11 +112,12 @@ return {
 			local servers = {
 				gopls = {},
 				html = {},
-				denols = {
-					root_dir = require("lspconfig").util.root_pattern({ "deno.json", "deno.jsonc" }),
-					single_file_support = false,
-					settings = {},
-				},
+				ts_ls = {},
+				-- denols = {
+				-- 	root_dir = require("lspconfig").util.root_pattern({ "deno.json", "deno.jsonc" }),
+				-- 	single_file_support = false,
+				-- 	settings = {},
+				-- },
 				tailwindcss = {},
 				cssls = {},
 				lua_ls = {
