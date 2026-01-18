d81e8731c6d7f349666d84d320629b3c791e5e15
Adam Maxwell <maxwell7774@gmail.com>
2026-01-18 15:37:07 -0700

    Updates Oil preferences

--- nvim/lua/stitch/lazy/oil.lua
+++ nvim/lua/stitch/lazy/oil.lua
@@ -4,6 +4,29 @@ return {
 	---@type oil.SetupOpts
 	opts = {
 		default_file_explorer = true,
+		keymaps = {
+			["g?"] = { "actions.show_help", mode = "n" },
+			["<CR>"] = "actions.select",
+			["<C-s>"] = { "actions.select", opts = { vertical = true } },
+			-- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+			["<C-t>"] = { "actions.select", opts = { tab = true } },
+			["<C-p>"] = "actions.preview",
+			["<leader>x"] = { "actions.close", mode = "n" },
+			["<C-c>"] = "actions.refresh",
+			["-"] = { "actions.parent", mode = "n" },
+			["_"] = { "actions.open_cwd", mode = "n" },
+			["`"] = { "actions.cd", mode = "n" },
+			["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+			["gs"] = { "actions.change_sort", mode = "n" },
+			["gx"] = "actions.open_external",
+			["g."] = { "actions.toggle_hidden", mode = "n" },
+			["g\\"] = { "actions.toggle_trash", mode = "n" },
+		},
+		-- Set to false to disable all of the above keymaps
+		use_default_keymaps = false,
+		view_options = {
+			show_hidden = true,
+		},
 	},
 	-- Optional dependencies
 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
